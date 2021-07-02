730d0c118be7463d615e1645a719ef919b97b1a9
lisswart <arnoudlisa@gmail.com>
2021-07-02 12:50:37 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        return "#{array[0]} and #{array[1]}"   
+    elsif array.length > 2
+        array[-1].insert(0, "and ")
+    end
+    array.join(", ")
 end
+
+puts oxford_comma(["kiwi", "durian"])
+puts oxford_comma(["rambutan", "passionfruit", "clementine"])
+puts oxford_comma(["butterfly"])
